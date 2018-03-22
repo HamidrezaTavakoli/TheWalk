153f942459af54dc93b77d3f1ffd99caa1f9b3c1
Hamid Tavakoli <hrt_v@yahoo.com>
2018-03-22 13:52:31 -0400

    Greeting and Turotial functions are added.

--- thewalk.py
+++ thewalk.py
@@ -2,5 +2,19 @@
 # This is a console text based game.
 # the concept is still under development
 
-print('Random print')
-print('I love Hamid!')
+
+def greetUser():
+    print('welcome\nHere you will walk the path of your destiny.')
+
+
+def tutorial():
+    print('In this game you will roll the dice and move.')
+    print('At the end of each move you are presented with a surprise.')
+    print('Surprises are, Enemies, Weapons, Food and Rooms.')
+    print('Food will regenarate a limited amount of health.')
+    print("""Choosing which room you want to step in will determine your
+        destiny.""")
+    print("""In case of gettng killed in a battle with an enemy
+        the Grim Reaper will come for your soul.
+        You can beg him for another chance and he might give you one.""")
+    print('Good Luck and enjoy playing')
