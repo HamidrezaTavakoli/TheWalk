0f44bb4448d80760a5544177e9497ca4f6e0f2a9
Ayla Parsons <aylaparsons@gmail.com>
2018-03-25 13:46:21 -0400

    food factory created

--- food.py
+++ food.py
@@ -17,3 +17,16 @@ class Meat(Food):
 class Water(Food):
     def __init__(self):
         super.__init__(self, 'water', 40)
+
+
+class FoodFactory():
+
+    def getFood(code):
+        if code == 1:
+            return Fruit()
+        elif code == 2:
+            return Meat()
+        elif code == 3:
+            return Water()
+        else:
+            print('{} is not a valid food code.'.fomrat(code))
